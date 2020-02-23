39ba41ab7ba5e3aaa95512e8427c9622d4c181a5
zhaojishun <1207296994@qq.com>
2020-02-23 21:38:44 +0800

    删除图床 ， 增加计算器样式

--- tuchuang/assets/num.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin:*');
-header('Content-type:application/json; charset=utf-8');
-error_reporting(0);
-$num = intval(file_get_contents("num.txt"));
-exit(json_encode([
-    "num"=>$num
-]));
--- tuchuang/images/img.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-
-$img_array = glob('se/*.{gif,jpg,png,jpeg,webp,bmp}', GLOB_BRACE);
-foreach ($img_array as $v){
-    echo "https://img.oioweb.cn/".$v."<br>";
-}
